a134dc575583199f1a23cfa8990577c70ce9c69f
ester-bloch <eb3212846@gmail.com>
2025-05-13 01:06:38 +0300

    contact - footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "../components/nav/nav.component";
+import { ContactComponent } from "../components/contact/contact.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
