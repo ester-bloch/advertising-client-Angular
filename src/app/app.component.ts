4c2c81cefa071d128248a4aa9de88c39ecf6258e
EstiZelt <e0527185161@gmail.com>
2025-05-14 18:50:35 +0300

    context

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "../components/nav/nav.component";
 import { ContactComponent } from "../components/contact/contact.component";
+import { FooterComponent } from "../components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, ContactComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
